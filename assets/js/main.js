d081fb0dfafdd910ac84d76dd2ddcf33480cf295
Edwin Langley <admin@edwinlangley.co.uk>
2021-02-28 13:35:57 +0000

    Added robots.txt

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,10 +43,10 @@
 			anchor: 'middle'
 		});
 
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return (breakpoints.active('<=mobile') ? 70 : 190); }
-		});
+		// $('.scrolly').scrolly({
+		// 	speed: 1000,
+		// 	offset: function() { return (breakpoints.active('<=mobile') ? 70 : 190); }
+		// });
 
 	// Parallax background.
 
